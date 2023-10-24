d6ab552d971f6314015858d19ca7520f3f86753a
Arterning <ning.huang.ridson@gmail.com>
2023-10-24 20:35:06 +0800

    Fix build error
    React报错之组件不能作为JSX组件使用
    https://cloud.tencent.com/developer/article/2077365

--- lib/schema.ts
+++ lib/schema.ts
@@ -13,7 +13,7 @@ export interface Database {
                 Row: {
                     id: number
                     inserted_at: string
-                    is_complete: boolean | null
+                    is_complete: boolean | undefined
                     task: string | null
                     user_id: string
                 }
